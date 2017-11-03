565a3156c7893480a99d0eee3cf8812e31c3ef27
digitopoi <hubo85@gmail.com>
2017-11-02 20:22:59 -0500

    Refactor list item to its own component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../recipe.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class RecipeItemComponent {
+  @Input() recipe: Recipe;
 
 }
