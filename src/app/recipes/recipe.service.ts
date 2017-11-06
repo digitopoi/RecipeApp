20e98ae89702926e1af0c529f8dca93e25d2e3ef
digitopoi <hubo85@gmail.com>
2017-11-06 15:46:18 -0600

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 import { Injectable, EventEmitter } from '@angular/core';
 
@@ -9,11 +10,21 @@ export class RecipeService {
         new Recipe(
             'A Test Recipe',
             'This is simply a test',
-            'https://c.pxhere.com/photos/c2/b0/eat_cheese_kohlrabi_tomatoes_au_gratin_meal_olives_cook-1323649.jpg!d'),
+            'https://c.pxhere.com/photos/c2/b0/eat_cheese_kohlrabi_tomatoes_au_gratin_meal_olives_cook-1323649.jpg!d',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French fries', 20)
+            ]
+        ),
         new Recipe(
             'Another Test Recipe',
             'This is simply a test',
-            'https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d')
+            'https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
+        )
       ];
 
       getRecipes() {
